9775803e6319e421a3100773af22bd54377c4e88
Adrian Quek <softwaredjinn@softwaredjinn.com>
2016-12-28 18:05:03 +0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to blitzcheat-ml
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject blitzcheat-ml "0.1.0-SNAPSHOT"
+  :description "Bejeweled Blitz bot that learns to play the game using machine learning."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.2.0"]]
+  :main ^:skip-aot blitzcheat-ml.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
