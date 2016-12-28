e53a0d945aee1daf87c2f725fef26213c92957c8
Pirateninjawizard <adrianqsk@gmail.com>
2016-12-28 21:08:39 +0800

    Added vim.session to gitignore. Added note about wayland causing screenshot to fail.

--- src/blitzcheat_ml/utils.clj
+++ src/blitzcheat_ml/utils.clj
@@ -8,7 +8,9 @@
     (javax.imageio ImageIO)))
 
 (defn take-screenshot []
+  ; Note that this borks if you are using Gnome on Wayland. I had to switch to Xorg for it to work. https://bbs.archlinux.org/viewtopic.php?id=220820
   (let [screen (.getScreenSize (Toolkit/getDefaultToolkit)) 
         rt (new Robot)
         img (.createScreenCapture rt (new Rectangle (int (.getWidth screen)) (int (.getHeight screen))))]
+    ;TODO: we will need to return the image names
     (ImageIO/write img "jpg" (new File (str (System/currentTimeMillis) ".jpg")))))
