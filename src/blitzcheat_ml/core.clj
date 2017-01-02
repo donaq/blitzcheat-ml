f81d13b47d35779fca6dfb57b4266e2338e8b7b7
Pirateninjawizard <adrianqsk@gmail.com>
2017-01-02 23:08:13 +0800

    Added a websockets server.

--- src/blitzcheat_ml/core.clj
+++ src/blitzcheat_ml/core.clj
@@ -1,9 +1,16 @@
 (ns blitzcheat-ml.core
   (:gen-class)
-  (:require [blitzcheat-ml.utils :as utils]))
+  (:require [blitzcheat-ml.utils :as utils])
+  (:use [org.httpkit.server]))
+
+(defn handler [request]
+  (with-channel request channel
+    (on-close channel (fn [status] (println "channel closed: " status)))
+    (on-receive channel (fn [data] ;; echo it back
+                          (send! channel data)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (utils/take-screenshot)
-  (println "Hello, World!"))
+  ;(utils/take-screenshot)
+  (run-server handler {:port 9999}))
