668529a4d3d37b626ca4e05aa82f0e7e08a3cacb
Pirateninjawizard <adrianqsk@gmail.com>
2018-08-05 20:46:42 +0800

    added pic navigation. class selector is not working properly yet

--- public/site.js
+++ public/site.js
@@ -7,24 +7,36 @@ $(document).ready(function(){
             pic = picdat.pics[k],
             img = document.createElement("IMG"),
             container = document.getElementById("imgcontainer");
+        console.log(JSON.stringify(pic));
 
         curr = i;
 
         // adding the image
-        $(".image").empty();
+        $(".image").remove();
         img.src = `/raw/${k}`;
         img.className = "image";
         img.id = `image${i}`;
         container.appendChild(img);
         $("#imgname").html(`${k}`);
+        if($.isEmptyObject(pic)) return;
 
+        // add exising image class
+        var pclass = ("class" in pic)?pic.class:"";
+        $("#imageclass").val(pclass);
+        $("#classes").val(pclass);
     }
 
     // populate classification selector
     function populate_classes(){
-        $(".classopt").empty();
+        $(".classopt").remove();
         var classes = Object.keys(picdat.classes).sort(),
             cls_sel = document.getElementById("classes");
+
+        var o = document.createElement("option");
+        o.className = "classopt";
+        o.text = "";
+        o.value = "";
+        cls_sel.appendChild(o);
 
         classes.forEach(c => {
             var o = document.createElement("option");
@@ -44,6 +56,29 @@ $(document).ready(function(){
             success: function(){ console.log("success!"); }
         });
     }
+
+    function savepic(){
+        var cls = $("#imageclass").val();
+        // only do something if we are initialised, we have a value to work with, and we have a pic
+        if(!picdat || !cls || cls=="" || curr==-1 || !(curr in sortedkeys)) return;
+
+        var k = sortedkeys[curr];
+        // set current pic's class
+        picdat.pics[k].class = cls;
+
+        // add this class if it doesn't exist
+        if(!(cls in picdat.classes)){
+            picdat.classes[cls] = {};
+            populate_classes();
+        }
+
+        save();
+    }
+
+	function modulo(a,b){
+		var r = a % b;
+		return r<0?b+r:r;
+	}
 
     // load pics
     $.getJSON("/ls", function(dat){
@@ -66,25 +101,19 @@ $(document).ready(function(){
         loadimg(0);
     });
 
+    $("#next").click(function(){
+        if(picdat==null) return;
+        loadimg(modulo(curr+1, sortedkeys.length));
+    });
+
+    $("#prev").click(function(){
+        if(picdat==null) return;
+        loadimg(modulo(curr-1, sortedkeys.length));
+    });
+
     $("#classes").change(evt => {
         $("#imageclass").val($("#classes").val());
     });
 
-    $("#setclass").click(function(){
-        var cls = $("#imageclass").val();
-        // only do something if we are initialised, we have a value to work with, and we have a pic
-        if(!picdat || !cls || cls=="" || curr==-1 || !(curr in sortedkeys)) return;
-
-        var k = sortedkeys[curr];
-        // set current pic's class
-        picdat.pics[k].class = cls;
-
-        // add this class if it doesn't exist
-        if(!(cls in picdat.classes)){
-            picdat.classes[cls] = {};
-            populate_classes();
-        }
-
-        save();
-    });
+    $("#setclass").click(savepic);
 });
